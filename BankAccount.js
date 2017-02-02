2a594d26d53a964a7fe302732ae5674fc016d6e6
Eri Irawan <eryirawan91@gmail.com>
2017-02-02 12:50:34 +0700

    release 0 & 1 is done

--- BankAccount.js
+++ BankAccount.js
@@ -4,9 +4,30 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    this._customerName = customer_name
-    this._type         = type
+    // ACCT_NUMBER.set(this. acct_number);
+    this.customerName  = customer_name
+    this.type          = type
     this._accNumber    = acct_number
+  }
+
+  setCustomeName(valName){
+    this.customerName = valName
+  }
+
+  getCustomerName(){
+    return this.customerName
+  }
+
+  setType(valType){
+    this.type = valType
+  }
+
+  getType(){
+    return this.type
+  }
+
+  setAccNumber(valNumber){
+    this._accNumber = valNumber
   }
 
   get account_number () {
@@ -14,11 +35,11 @@ class BankAccount {
   }
 
   to_string () {
-    //
+    return `${this.customerName}: ${this.type}# ${this._accNumber}`
   }
 
   cover_digits () {
-    //
+    return this._accNumber.replace(/(\d{3})-(\d{3})-(\d{3})/, "***-***-$3")
   }
 }
 
@@ -31,4 +52,5 @@ console.log(my_acct.account_number)
 my_acct.to_string() // "Hacktivate: Checking# 333-555-888"
 
 // release 1
-my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
+ // "Hacktivate: Checking# ***-***-888"
+console.log(my_acct.cover_digits());

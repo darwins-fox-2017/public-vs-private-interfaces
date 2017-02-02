a55fa783a84001cd4dfb9d76f6e1fde875b7d85f
adamsaparudin <adamsaparudin@gmail.com>
2017-02-02 10:25:37 +0700

    HORE SELESAI

--- BankAccount.js
+++ BankAccount.js
@@ -4,19 +4,22 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this.customer_name = customer_name
+    this.type = type
+    this._acct_number = acct_number
   }
 
   get account_number () {
-    //
+    return this._acct_number
   }
 
   to_string () {
-    //
+    return `${this.customer_name}, ${this.type}, ${this._acct_number}`
   }
 
   cover_digits () {
-    //
+    let cover = this._acct_number.replace(/\d{3}-\d{3}/, "***-***")
+    return cover
   }
 }
 
@@ -26,7 +29,7 @@ console.log(my_acct)
 console.log(my_acct.account_number)
 
 // release 0
-my_acct.to_string() // "Hacktivate: Checking# 333-555-888"
+console.log(my_acct.to_string()) // "Hacktivate: Checking# 333-555-888"
 
 // release 1
-my_acct.cover_digits() // "Hacktivate: Checking# ***-***-888"
+console.log(my_acct.cover_digits()) // "Hacktivate: Checking# ***-***-888"

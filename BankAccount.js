a8080ec47ab7799e6978c181dfab558ab2ebb817
Samuel PH Sinaga & Triyan Arief Wibowo <pair+samuelphsinaga.triyanarief@hacktiv8.com>
2017-02-02 11:12:44 +0700

    pub-private done

--- BankAccount.js
+++ BankAccount.js
@@ -4,26 +4,32 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this._customer_name = customer_name
+    this._type = type
+    this._acct_number = acct_number
   }
 
   get account_number () {
-    //
+    return this._acct_number;
   }
 
   to_string () {
-    //
+    var print = `${this._customer_name}: ${this._type}# ${this._acct_number}`
+    console.log(print)
   }
 
   cover_digits () {
-    //
+    let regex = this._acct_number.replace(/(\d{3})-(\d{3})-(\d{3})/g,"***-***-$3")
+    console.log(this._customer_name+ ": " +this._type+"# " +regex)
   }
 }
 
 let my_acct = new BankAccount('Hacktivate', 'Checking', '333-555-888')
 
-console.log(my_acct)
-console.log(my_acct.account_number)
+// console.log(my_acct)
+// console.log(my_acct.account_number)
+// console.log(my_acct.cover_digits)
+
 
 // release 0
 my_acct.to_string() // "Hacktivate: Checking# 333-555-888"

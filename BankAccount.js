d8f418d7373ff9899a6882de6297d0e6340b96f5
isumizumi <isumi.karina72@gmail.com>
2017-02-02 11:14:47 +0700

    Finish

--- BankAccount.js
+++ BankAccount.js
@@ -4,19 +4,23 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this._customer_name = customer_name
+    this._type = type
+    this._acct_number = acct_number
   }
 
   get account_number () {
-    //
+    return this._acct_number
   }
 
   to_string () {
-    //
+    console.log(`${this._customer_name}: ${this._type}# ${this._acct_number}`);
+    return this
   }
 
   cover_digits () {
-    //
+    console.log(`${this._customer_name}: ${this._type}# ${this._acct_number.replace(/\d{3}-\d{3}/, "***-***")}`);
+    return this
   }
 }
 

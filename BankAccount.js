4232b00a8e6b49d1e304fe06c92063f391f913d0
Diky Arga <dikyarga.id@gmail.com>
2017-02-02 10:29:10 +0700

    Version 0 released

--- BankAccount.js
+++ BankAccount.js
@@ -5,14 +5,20 @@ const ACCT_NUMBER = new WeakMap()
 class BankAccount {
   constructor (customer_name, type, acct_number) {
     //
+    this._customer_name = customer_name
+    this._type = type
+    this._acct_number = acct_number
   }
 
   get account_number () {
+    return this._acct_number
     //
   }
 
   to_string () {
     //
+    console.log(`${this._customer_name}: ${this._type}# ${this._acct_number}`);
+    return `${this._customer_name}: ${this._type}# ${this._acct_number}`
   }
 
   cover_digits () {

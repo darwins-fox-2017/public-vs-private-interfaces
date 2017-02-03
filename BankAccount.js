389f5168e720da9e6a90aca3cad4f32b51ee9e39
Agindo <ramadhanagindo@gmail.com>
2017-02-03 09:32:36 +0700

    Udah selesai

--- BankAccount.js
+++ BankAccount.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const ACCT_NUMBER = new WeakMap()
-
 class BankAccount {
   constructor (customer_name, type, acct_number) {
     this.customer_name = customer_name;
@@ -9,25 +7,25 @@ class BankAccount {
     this._acct_number = acct_number;
   }
 
-  get account_number () {
+  get acct_number() {
     return `${this._acct_number}`
   }
 
   to_string () {
-    console.log(`${this.customer_name}: ${this.type}# ${this._acct_number}`)
+    console.log(`${this.customer_name}: ${this.type} #${this._acct_number}`)
     return this
   }
 
   cover_digits () {
-    console.log(`${this.customer_name}: ${this.type}# ${this._acct_number.replace(/333-555-888/i, "***-***-888")}`)
+    console.log(`${this.customer_name}: ${this.type}# ${this._acct_number.replace(/(\d\d\d)-(\d\d\d)-(\d\d\d)/i, "***-***-$3")}`)
     return this
   }
 }
 
-let my_acct = new BankAccount('Hacktivate', 'Checking', '333-555-888')
+let my_acct = new BankAccount('Hacktivate', 'Checking', '444-555-777')
 
 console.log(my_acct)
-console.log(my_acct.account_number)
+console.log(my_acct.acct_number)
 
 // release 0
 my_acct.to_string() // "Hacktivate: Checking# 333-555-888"

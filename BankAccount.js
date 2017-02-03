3b686ba6e14cdb48dfb2ab6ecffbb256927dd37d
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-03 10:39:08 +0700

    Public vs Private interface Danang

--- BankAccount.js
+++ BankAccount.js
@@ -5,14 +5,39 @@ const ACCT_NUMBER = new WeakMap()
 class BankAccount {
   constructor (customer_name, type, acct_number) {
     //
+    this._customer_name = customer_name
+    this._type = type
+    this._account_number = acct_number
+  }
+
+  set customer_name (name) {
+    this._customer_name = name
+  }  
+
+  get customer_name () {
+    return this._customer_name
+  }
+
+  set type (type) {
+    this._type = type
+  }
+
+  get type () {
+    return this._type
+  }
+  
+  set account_number (acc) {
+    this._account_number = acc
   }
 
   get account_number () {
     //
+    return this._account_number
   }
 
   to_string () {
     //
+    console.log(`Hacktivate: Checking# ${this.account_number}`)
   }
 
   cover_digits () {
@@ -24,6 +49,10 @@ let my_acct = new BankAccount('Hacktivate', 'Checking', '333-555-888')
 
 console.log(my_acct)
 console.log(my_acct.account_number)
+
+// my_acct.customer_name = 'tes'
+// console.log(my_acct.customer_name)
+
 
 // release 0
 my_acct.to_string() // "Hacktivate: Checking# 333-555-888"

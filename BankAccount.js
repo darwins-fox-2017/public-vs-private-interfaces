0b250bb73252c757d60ed94a3273b4db888ce829
pisanggoreng <7ofpentacles@gmail.com>
2017-02-05 23:33:58 +0700

    clear

--- BankAccount.js
+++ BankAccount.js
@@ -1,22 +1,32 @@
 'use strict'
 
+// const costumerName = new WeakMap();
+// const type = new WeakMap()
+
 const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this.costumerName = customer_name;
+    this.type = type;
+    ACCT_NUMBER.set(this, acct_number)
+  }
+
+
+  set setAcctNumber(acct_number) {
+    ACCT_NUMBER.set(this, acct_number)
   }
 
   get account_number () {
-    //
+    return ACCT_NUMBER.get(this)
   }
 
   to_string () {
-    //
+    console.log( `${this.costumerName}: ${this.type}# ${ACCT_NUMBER.get(this)}` );
   }
 
   cover_digits () {
-    //
+    console.log( `${this.costumerName}: ${this.type}# ${ACCT_NUMBER.get(this) .replace(/\d{3}.\d{3}/,"***-***")}` );
   }
 }
 
